3ea52b1c4831896d00d2bdb6f663e6fec020a4e1
Noa <noal@student.chalmers.se>
2021-03-28 18:05:49 +0200

    Indy can no longer spawn in a cell

--- indymanager.py
+++ indymanager.py
@@ -7,7 +7,6 @@ try:
 except ModuleNotFoundError:
     print("You need to install networkx. Try the command: pip3 install networkx")
 
-#TODO: Make it so that Indy can't spawn in a wall
 
 class IndyManager:
     def __init__(self, n_row, n_col, maze:Maze, n_indies = 2):
@@ -19,7 +18,14 @@ class IndyManager:
         
         self.G = nx.Graph()
         self.G_set = set()
-        self.indies = [Indy(np.random.randint(0,n_row),np.random.randint(0,n_col), maze, self.available_jobs, self.indy_pos_list, self.cells, self.G, self.G_set) for i in range(n_indies)]
+        self.indies = []
+        for _ in range(n_indies):
+            done = False
+            while not done:
+                row_i, col_i = np.random.randint(0,n_row),np.random.randint(0,n_col)
+                done = maze.cells[(row_i, col_i)].state == 'available'  
+
+            self.indies.append(Indy(row_i, col_i, maze, self.available_jobs, self.indy_pos_list, self.cells, self.G, self.G_set)) 
 
         self.past_drawable_dicts = []
         self.past_drawable_dicts.append(self.generate_drawable_dict())
